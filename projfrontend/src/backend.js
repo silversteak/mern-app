3142846b6446e31634fcc15a84d544839487dc43
Tauseef Rahaman <tauseef.rahaman@yahoo.com>
2020-11-20 10:12:46 +0530

    fix on PORT v2

--- projfrontend/src/backend.js
+++ projfrontend/src/backend.js
@@ -1,4 +1,5 @@
 console.log("The environment is " + process.env.NODE_ENV);
+console.log("The port is " + process.env.REACT_PRODUCTION_PORT);
 export const PORT =
   process.env.NODE_ENV === "production"
     ? process.env.REACT_PRODUCTION_PORT
