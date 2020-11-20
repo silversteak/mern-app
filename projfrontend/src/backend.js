8e6deee15572460ec6f5188b1b8daba243ffbd9d
Tauseef Rahaman <tauseef.rahaman@yahoo.com>
2020-11-20 10:29:09 +0530

    Hard code Environment

--- projfrontend/src/backend.js
+++ projfrontend/src/backend.js
@@ -2,5 +2,5 @@ console.log("The environment is " + process.env.NODE_ENV);
 console.log("The port is " + process.env.REACT_PRODUCTION_PORT);
 export const PORT =
   process.env.NODE_ENV === "production"
-    ? process.env.REACT_PRODUCTION_PORT
-    : process.env.REACT_APP_PORT;
+    ? "https://ninja-thrones.herokuapp.com"
+    : "http://localhost:8000";
